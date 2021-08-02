1e9ea833b26462192de80e4f074e7e84d5092e7e
koo9water <koo9water@gmail.com>
2021-08-02 17:43:27 +0900

    Uploaded.

---
+++ libs/dp.py
@@ -0,0 +1,227 @@
+import cv2
+import sys
+import os
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+sys.path.append(os.path.abspath(os.getcwd()))
+import libs.utils
+
+import matplotlib.pyplot as plt
+
+class GenerateHeatmap():
+    def __init__(self, output_res, num_parts):
+        self.output_res = output_res
+        self.num_parts = num_parts
+        sigma = self.output_res/64
+        self.sigma = sigma
+        size = 6 * sigma + 3
+        x = np.arange(0, size, 1, float)
+        y = x[:, np.newaxis]
+        x0, y0 = 3 * sigma + 1, 3 * sigma + 1
+        self.g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
+
+    def __call__(self, keypoints):
+        # from keypoints, return heatmaps
+        hms = np.zeros(shape=(self.output_res, self.output_res,
+                       self.num_parts), dtype=np.float32)
+        sigma = self.sigma
+
+        for idx, pt in enumerate(keypoints):
+            if pt[0] > 0:
+                x, y = int(pt[0]), int(pt[1])
+                if x < 0 or y < 0 or x >= self.output_res or y >= self.output_res:
+                    continue
+                ul = int(x - 3 * sigma - 1), int(y - 3 * sigma - 1)
+                br = int(x + 3 * sigma + 2), int(y + 3 * sigma + 2)
+
+                c, d = max(0, -ul[0]), min(br[0], self.output_res) - ul[0]
+                a, b = max(0, -ul[1]), min(br[1], self.output_res) - ul[1]
+
+                cc, dd = max(0, ul[0]), min(br[0], self.output_res)
+                aa, bb = max(0, ul[1]), min(br[1], self.output_res)
+                hms[aa:bb, cc:dd, idx] = np.maximum(
+                    hms[aa:bb, cc:dd, idx], self.g[a:b, c:d])
+        return hms
+
+class Dataset():
+    def __init__(self, img_res, hms_res, num_parts, csv_dir):
+        self.img_res = img_res
+        self.hms_res = hms_res
+        self.num_parts = num_parts
+        self.csv_dir = csv_dir
+        self.base_dir = os.path.dirname(self.csv_dir)
+        self.load_datalist(self.csv_dir)
+        self.idx_list = np.arange(len(self.train_list))
+
+        self.generateHeatmap = GenerateHeatmap(self.hms_res, num_parts)
+
+    def load_datalist(self, csv_dir):
+        self.df = pd.read_csv(csv_dir)
+        del self.df['Unnamed: 0']
+        self.train_list = self.df.values.tolist()
+        print(f'Train dataset: {self.csv_dir}')
+        print(f'Train dataset is loaded. Shape: {self.df.shape}')
+
+    def get_path(self, path):
+        return os.path.join(self.base_dir, path)
+
+    def get_label(self, label_file):
+        kps = np.genfromtxt(self.get_path(label_file))
+        top = kps[:,1].min()
+        right = kps[:,0].max()
+        bottom = kps[:,1].max()
+        left = kps[:,0].min()
+        bb = np.array([left, top, right, bottom])
+        center = [bb[2] - (bb[2] - bb[0]) / 2.0, bb[3] - (bb[3] - bb[1]) / 2.0]
+        scale = (bb[2] - bb[0] + bb[3] - bb[1]) / 256 # 195
+        return kps, center, scale
+
+    def get_img(self, image_file):
+        img_path = self.get_path(image_file)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = tf.io.read_file(img_path)
+        # img = tf.image.decode_jpeg(img, channels=3)
+        return img
+
+    def load_image(self, idx):
+        ## load + crop
+        image_file, label_file = self.train_list[idx]
+        orig_img = self.get_img(image_file)
+        orig_kps, center, scale = self.get_label(label_file)
+        
+        orig_img, orig_kps = self.random_rotate(orig_img, orig_kps, center)
+        
+        orig_img = self.random_color_augmentation(orig_img)
+
+        cropped = libs.utils.crop_and_normalize(orig_img, center, scale)
+        heatmaps = libs.utils.create_target_heatmap(orig_kps, center, scale)
+
+        # heatmap = np.zeros((self.hms_res, self.hms_res))
+        # for i in range(heatmaps.shape[2]):
+        #     heatmap += heatmaps[:,:,i]
+        # heatmap = heatmap.reshape((heatmap.shape[0], heatmap.shape[1], 1))
+
+        # plt.imshow(heatmap)
+        # plt.savefig('heatmaps')
+        # plt.show()
+    
+        # plt.imshow(cropped)
+        # plt.savefig('result')
+        # plt.show()
+        # print(cropped.max())
+
+        return cropped.astype(np.float32), heatmaps.astype(np.float32)
+
+    def load_eval(self, idx):
+        image_file, label_file = self.train_list[idx]
+        orig_img = self.get_img(image_file)
+        orig_kps, center, scale = self.get_label(label_file)
+        
+        # orig_img, orig_kps = self.random_rotate(orig_img, orig_kps, center)
+        
+        # orig_img = self.random_color_augmentation(orig_img)
+
+        cropped = libs.utils.crop_and_normalize(orig_img, center, scale)
+        kps = libs.utils.create_target_landmarks(orig_kps, center, scale, 64)
+
+        return cropped, kps, center, scale
+
+    def rotate_landmarks(self, landmarks, angle, center):
+        radian = np.radians(angle)
+        rotated_landmarks = []
+        for i in landmarks:
+            _x = i[0] - center[0]
+            _y = i[1] - center[1]
+            x = _x * np.cos(radian) - _y * np.sin(radian)
+            y = _x * np.sin(radian) + _y * np.cos(radian)
+            x += center[0]
+            y += center[1]
+            rotated_landmarks.append([x, y])
+
+        return np.array(rotated_landmarks, dtype='float32')
+
+    def random_rotate(self, image, kps, center):
+        src = image
+        angle = np.random.uniform(-5., 5.)
+        # print(f'angle: {angle}')
+        height, width, channel = src.shape
+        matrix = cv2.getRotationMatrix2D(tuple(center), -angle, 1)
+        dst = cv2.warpAffine(src, matrix, (width, height))
+        rotated_landmarks = self.rotate_landmarks(kps, angle, center)
+
+        return dst, rotated_landmarks
+
+    def random_color_augmentation(self, src):
+        hsv = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)
+        h, s, v = cv2.split(hsv)
+        i = (np.random.random() - 0.5) * 0.8
+        s = s * (1 - i)
+        s = np.clip(s, 0, 255)
+        s = np.ndarray.astype(s, 'uint8')
+
+        merged = cv2.merge([h, s, v])
+        dst = cv2.cvtColor(merged, cv2.COLOR_HSV2RGB)
+
+        a = 1.0 + (np.random.random() - 0.5) * 0.4
+        b = np.random.randint(-20, 20)
+
+        dst = dst * a + b
+        dst = np.clip(dst, 0, 255)
+
+        return dst
+
+    def gen(self): 
+        np.random.shuffle(self.idx_list)
+        for idx in self.idx_list:
+            img, heatmaps = self.load_image(idx)
+            yield img, heatmaps
+
+    def gen_eval(self):
+        for idx in self.idx_list:
+            image, landmarks, center, scale = self.load_eval(idx)
+            yield image, landmarks, center, scale
+
+    def tf_dataset_from_generator(self, batch_size):
+        return tf.data.Dataset.from_generator(
+            self.gen,
+            output_types=(
+                np.float32, np.float32),
+            output_shapes=(
+                [self.img_res, self.img_res, 3],
+                [self.hms_res, self.hms_res, self.num_parts]),
+            args=()
+        ).repeat().batch(batch_size)
+
+    # for VGG-16
+    def load_with_no_heatmaps(self, idx):
+        image_file, label_file = self.train_list[idx]
+        orig_img = self.get_img(image_file)
+        orig_kps, center, scale = self.get_label(label_file)
+        
+        orig_img, orig_kps = self.random_rotate(orig_img, orig_kps, center)
+        
+        orig_img = self.random_color_augmentation(orig_img)
+
+        cropped = libs.utils.crop_and_normalize(orig_img, center, scale)
+        kps = libs.utils.create_target_landmarks(orig_kps, center, scale, 256)
+        kps = kps / 256.
+
+        return cropped.astype(np.float32), kps.astype(np.float32)
+
+    def gen_vgg16(self):
+        np.random.shuffle(self.idx_list)
+        for idx in self.idx_list:
+            img, kps = self.load_with_no_heatmaps(idx)
+            np_kps = np.zeros((136), dtype=np.float32)
+            for i in range(68):
+                np_kps[i*2] = kps[i][0]
+                np_kps[i*2+1] = kps[i][1]
+            yield img, np_kps
+
+
+if __name__ == "__main__":
+    # dataset = Dataset(256, 64, 68, '../Data/300W_train/train.csv')
+    # dataset.load_image(4)
+    dataset = Dataset(256, 64, 68, '../Data/300W/eval.csv')
---
+++ libs/utils.py
@@ -0,0 +1,151 @@
+import numpy as np
+import cv2
+import math
+
+def get_transform(center, scale, res, rot=0):
+    # Generate transformation matrix
+    h = 200 * scale
+    t = np.zeros((3, 3))
+    t[0, 0] = float(res[1]) / h
+    t[1, 1] = float(res[0]) / h
+    t[0, 2] = res[1] * (-float(center[0]) / h + .5)
+    t[1, 2] = res[0] * (-float(center[1]) / h + .5)
+    t[2, 2] = 1
+    if not rot == 0:
+        rot = -rot # To match direction of rotation from cropping
+        rot_mat = np.zeros((3,3))
+        rot_rad = rot * np.pi / 180
+        sn,cs = np.sin(rot_rad), np.cos(rot_rad)
+        rot_mat[0,:2] = [cs, -sn]
+        rot_mat[1,:2] = [sn, cs]
+        rot_mat[2,2] = 1
+        # Need to rotate around center
+        t_mat = np.eye(3)
+        t_mat[0,2] = -res[1]/2
+        t_mat[1,2] = -res[0]/2
+        t_inv = t_mat.copy()
+        t_inv[:2,2] *= -1
+        t = np.dot(t_inv,np.dot(rot_mat,np.dot(t_mat,t)))
+    return t
+
+def transform(pt, center, scale, res, invert=0, rot=0):
+    # Transform pixel location to different reference
+    t = get_transform(center, scale, res, rot=rot)
+    if invert:
+        t = np.linalg.inv(t)
+    new_pt = np.array([pt[0], pt[1], 1.]).T
+    new_pt = np.dot(t, new_pt)
+    return new_pt[:2].astype(int)
+
+def crop(img, center, scale, res, rot=0):
+    # Upper left point
+    ul = np.array(transform([0, 0], center, scale, res, invert=1))
+    # Bottom right point
+    br = np.array(transform(res, center, scale, res, invert=1))
+
+    new_shape = [br[1] - ul[1], br[0] - ul[0]]
+    if len(img.shape) > 2:
+        new_shape += [img.shape[2]]
+    new_img = np.zeros(new_shape)
+
+    # Range to fill new array
+    new_x = max(0, -ul[0]), min(br[0], len(img[0])) - ul[0]
+    new_y = max(0, -ul[1]), min(br[1], len(img)) - ul[1]
+    # Range to sample from original image
+    old_x = max(0, ul[0]), min(len(img[0]), br[0])
+    old_y = max(0, ul[1]), min(len(img), br[1])
+    new_img[new_y[0]:new_y[1], new_x[0]:new_x[1]] = img[old_y[0]:old_y[1], old_x[0]:old_x[1]]
+
+    return cv2.resize(new_img, res)
+
+def create_bounding_box(target_landmarks, expansion_factor=0.0):
+    """
+    gets a batch of landmarks and calculates a bounding box that includes all the landmarks per set of landmarks in
+    the batch
+    :param target_landmarks: batch of landmarks of dim (n x 68 x 2). Where n is the batch size
+    :param expansion_factor: expands the bounding box by this factor. For example, a `expansion_factor` of 0.2 leads
+    to 20% increase in width and height of the boxes
+    :return: a batch of bounding boxes of dim (n x 4) where the second dim is (x1,y1,x2,y2)
+    """
+    # Calc bounding box
+    x_y_min = np.reshape(target_landmarks, (-1, 209, 2)).min(axis=1)
+    x_y_max = target_landmarks.reshape(-1, 209, 2).max(axis=1)
+    # expanding the bounding box
+    expansion_factor /= 2
+    bb_expansion_x = (x_y_max[:, 0] - x_y_min[:, 0]) * expansion_factor
+    bb_expansion_y = (x_y_max[:, 1] - x_y_min[:, 1]) * expansion_factor
+    x_y_min[:, 0] -= bb_expansion_x
+    x_y_max[:, 0] += bb_expansion_x
+    x_y_min[:, 1] -= bb_expansion_y
+    x_y_max[:, 1] += bb_expansion_y
+    
+    return np.concatenate((x_y_min, x_y_max), axis=1)
+
+def _gaussian(
+        size=3, sigma=0.25, amplitude=1, normalize=False, width=None,
+        height=None, sigma_horz=None, sigma_vert=None, mean_horz=0.5,
+        mean_vert=0.5):
+    # handle some defaults
+    if width is None:
+        width = size
+    if height is None:
+        height = size
+    if sigma_horz is None:
+        sigma_horz = sigma
+    if sigma_vert is None:
+        sigma_vert = sigma
+    center_x = mean_horz * width + 0.5
+    center_y = mean_vert * height + 0.5
+    gauss = np.empty((height, width), dtype=np.float32)
+    # generate kernel
+    for i in range(height):
+        for j in range(width):
+            gauss[i][j] = amplitude * math.exp(-(math.pow((j + 1 - center_x) / (
+                sigma_horz * width), 2) / 2.0 + math.pow((i + 1 - center_y) / (sigma_vert * height), 2) / 2.0))
+    if normalize:
+        gauss = gauss / np.sum(gauss)
+    return gauss
+
+def draw_gaussian(image, point, sigma):
+    # Check if the gaussian is inside
+    ul = [math.floor(point[0] - 3 * sigma), math.floor(point[1] - 3 * sigma)]
+    br = [math.floor(point[0] + 3 * sigma), math.floor(point[1] + 3 * sigma)]
+    if (ul[0] > image.shape[1] or ul[1] > image.shape[0] or br[0] < 1 or br[1] < 1):
+        return image
+    size = 6 * sigma + 1
+    g = _gaussian(size)
+    g_x = [int(max(1, -ul[0])), int(min(br[0], image.shape[1])) - int(max(1, ul[0])) + int(max(1, -ul[0]))]
+    g_y = [int(max(1, -ul[1])), int(min(br[1], image.shape[0])) - int(max(1, ul[1])) + int(max(1, -ul[1]))]
+    img_x = [int(max(1, ul[0])), int(min(br[0], image.shape[1]))]
+    img_y = [int(max(1, ul[1])), int(min(br[1], image.shape[0]))]
+    assert (g_x[0] > 0 and g_y[1] > 0)
+    image[img_y[0] - 1:img_y[1], img_x[0] - 1:img_x[1]
+          ] = image[img_y[0] - 1:img_y[1], img_x[0] - 1:img_x[1]] + g[g_y[0] - 1:g_y[1], g_x[0] - 1:g_x[1]]
+    image[image > 1] = 1
+    return image
+
+def create_target_heatmap(target_landmarks, centers, scales):
+    heatmaps = np.zeros((64, 64, 68), dtype=np.float32)
+    for p in range(68):
+        landmark_cropped_coor = transform(target_landmarks[p] + 1, centers, scales, (64, 64), invert=False)
+        heatmaps[:,:,p] = draw_gaussian(heatmaps[:,:,p], landmark_cropped_coor + 1, 1)
+    return heatmaps
+
+def create_target_landmarks(target_landmarks, center, scale, size):
+    landmarks = np.zeros(target_landmarks.shape)
+    for i, kp in enumerate(target_landmarks):
+        landmarks[i] = transform(kp, center, scale, (size, size))
+    return landmarks
+
+def make_bb(face_locations):
+    bb = []
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        bb.append([left, top, right, bottom])
+    return np.array(bb)
+
+def crop_and_normalize(image, center, scale):
+    cropped = crop(image, center, scale, (256, 256))
+    float_image = cropped.astype(np.float32)
+    normalized_image = float_image / 255.
+    return normalized_image
