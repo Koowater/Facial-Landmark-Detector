57ad3999d3d42eb7f81ce5ecb116204d9e962978
Koowater <koo9water@gmail.com>
2021-08-03 20:08:16 +0900

    Training code is added.

---
+++ train.py
@@ -0,0 +1,37 @@
+from tasks.model import FacialLandmarkDetector
+from libs.dp import Dataset
+import tensorflow as tf
+import pickle
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+print(tf.config.list_physical_devices('GPU'))
+
+model = FacialLandmarkDetector()
+
+# checkpoint
+
+checkpoint_path = "data/checkpoint_model/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path, 
+    verbose=1, 
+    save_weights_only=True,
+    save_freq='epoch')
+
+model.save_weights(checkpoint_path.format(epoch=0))
+
+IMG_RES = 256
+HMS_RES = 64
+NUM_LANDMARKS = 68
+BATCH_SIZE = 16
+DATASET_DIR = '../Data/300W_train/train.csv'
+
+dataset = Dataset(IMG_RES, HMS_RES, NUM_LANDMARKS, DATASET_DIR)
+dataset_generator = dataset.tf_dataset_from_generator(BATCH_SIZE)
+
+hist = model.fit(dataset_generator.take(240), callbacks=[cp_callback], epochs=100)
+
+with open('hist_SHG.pickle', 'wb') as f:
+    pickle.dump(hist, f)
