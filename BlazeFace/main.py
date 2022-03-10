b6709848c6cf56803ee2e9910a789ea9aa097737
Koowater <koo9water@gmail.com>
2022-03-10 23:17:59 +0900

    BlazeFace is added.

---
+++ BlazeFace/main.py
@@ -0,0 +1,113 @@
+import time
+import argparse
+
+from network import FeatureExtractor
+
+import tensorflow as tf
+from tensorflow.keras.layers import DepthwiseConv2D, Conv2D, MaxPooling2D, ReLU, Input, BatchNormalization, Reshape, Concatenate
+from tensorflow.keras.activations import relu
+
+class BlazeFace():
+    def __init__(self, config):
+        self.input_shape = config.input_shape
+        self.feature_extractor = FeatureExtractor(self.inpu_shape)
+        self.n_boxes [2, 6]
+        self.model = self.build_model()
+
+        if config.train:
+            self.batch_size = config.batch_size
+            self.epochs = config.epochs
+
+        self.checkpoint_path = config.checkpoint_path
+        self.total_data = config.total_data
+    
+    def build_model(self):
+        model = self.feature_extractor
+
+        # bounding box confidence prediction
+        bb_16_conf = Conv2D(filters=self.n_boxes[0] * 1,
+                                            kernel_size=3,
+                                            padding='same',
+                                            activation='sigmoid')(model.output[0])
+        bb_16_conf_reshaped = Reshape((16**2 * self.n_boxes[0], 1))(bb_16_conf)
+
+        bb_8_conf = Conv2D(filters=self.n_boxes[0] * 1,
+                           kernel_size=3,
+                           padding='same', 
+                           activation='sigmoid')(model.output[1])
+
+        bb_8_conf_reshaped = Reshape((8**2 * self.n_boxes[0], 1))(bb_8_conf)
+
+        conf_of_bb = Concatenate(axis=1)([bb_16_conf_reshaped, bb_8_conf_reshaped])
+
+        # bounding box location prediction
+        bb_16_loc = Conv2D()(filters=self.n_boxes[0] * 4,
+                             kernel_size=3,
+                             padding='same')(model.output[0])
+        
+        bb_16_loc_reshape = Reshape((16**2 * self.n_boxes[1], 4))(bb_16_loc)
+
+        bb_8_loc = Conv2D(filters=self.n_boxes[1] * 4,
+                          kernel_size=3,
+                          padding='same')(bb_8_loc)
+
+        bb_8_loc_reshape = Reshape((8**2 * self.n_boxes[1], 4))(bb_8_loc)
+
+        loc_of_bb = Concatenate(axis=1)([bb_16_loc_reshape[1], bb_8_loc_reshape])
+
+        output_combined = Concatenate(axis=-1)([conf_of_bb, loc_of_bb])
+
+        return tf.keras.models.Model(model.input, output_combined)
+
+    def train(self):
+        opt = tf.keras.optimizers.Adam(amsgrad=True) # amsgrad 사용 여부는 실험 후 정확도를 확인해보자.
+        model = self.model
+        model.compile(loss=['categorical_crossentropy', 'MAE'], optimizer=opt) # MAE 대신 smooth_l1_loss도 적용해보자.
+
+        tb = tf.keras.callbacks.TensorBoard(log_dir="./logs/", update_freq='batch')
+
+        STEP_SIZE_TRAIN = self.total_data // self.batch_size
+
+        t0 = time.time()
+
+        data_gen = dataloader(config.dataset_dir, config.label_path, self.batch_size)
+        
+        for epoch in range(self.epochs):
+            t1 = time.time()
+            hist = model.fit_generator(generator=data_gen,
+                                       steps_per_epoch=STEP_SIZE_TRAIN,
+                                       initial_epoch=epoch,
+                                       callbacks=[reduce_lr, tb],
+                                       verbose=1,
+                                       shuffle=True)
+
+            t2 = time.time()
+
+            print(hist.history)
+
+            print(f'Training time for one epoch : {t2 - t1:.3}')
+
+            if epoch % 100 == 0:
+                model.save_weights(os.path.join(config.checkpoint_path, str(epoch)))
+
+        print(f'Total training time : {time.time() - t0:.3}')
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    
+    # hyperparameters
+    args.add_argument('--input_shape', type=int, default=[128, 128, 3])
+    args.add_argument('--batch_size', type=int, default=128)
+    args.add_argument('--nb_epoch', type=int, default=1000)
+    args.add_argument('--numdata', type=int, default=2625)
+    args.add_argument('--train', type=bool, default=True)
+    args.add_argument('--checkpoint_path', type=str, default="./")
+    args.add_argument('--dataset_dir', type=str, default="./")
+    args.add_argument('--label_path', type=str, default="./")
+
+    config = args.parse_args()
+
+    blazeface = BlazeFace(config)
+
+    if config.train:
+        blazeface.train()
---
+++ BlazeFace/network.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from tensorflow.keras.layers import DepthwiseConv2D, Conv2D, MaxPooling2D, ReLU, Input, BatchNormalization, Reshape, Concatenate
+
+class BlazeBlock(tf.keras.Model):
+    def __init__(self, channel, stride=1, name='BlazeBlock'):
+        super(BlazeBlock, self).__init__(name=name)
+
+        self.stride = stride
+        if self.stride == 2:
+            self.pooling = MaxPooling2D(strides=self.stride)
+
+        self.dwconv = DepthwiseConv2D(kernel_size=5, strides=(self.stride, self.stride), padding='same')
+        self.pwconv = Conv2D(filters=channel, kernel_size=(1, 1))
+
+        self.bn = BatchNormalization()
+
+    def call(self, input_tensor):
+        residual = input_tensor
+        if self.stride == 2:
+            residual = self.pooling(residual)
+
+        x = self.dwconv(input_tensor)
+        x = self.pwconv(x)
+        x = self.bn(x)
+
+        if x.shape[-1] != residual.shape[-1]:
+            residual = tf.concat([residual, tf.zeros_like(residual)], axis=-1, name='channel_padding')
+            
+        x = relu(x + residual)
+        return x
+
+class DoubleBlazeBlock(tf.keras.Model):
+    def __init__(self, channel, stride=1, name='DoubleBlazeBlock'):
+        super(DoubleBlazeBlock, self).__init__(name=name)
+
+        self.stride = stride
+        if self.stride == 2:
+            self.pooling = MaxPooling2D(strides=self.stride)
+
+        self.dwconv_0 = DepthwiseConv2D(kernel_size=5, strides=(stride, stride), padding='same')
+        self.pwconv_0 = Conv2D(filters=24, kernel_size=(1, 1))
+        self.dwconv_1 = DepthwiseConv2D(kernel_size=5, strides=(1, 1), padding='same')
+        self.pwconv_1 = Conv2D(filters=channel, kernel_size=(1, 1))
+
+        self.bn_0 = BatchNormalization()
+        self.bn_1 = BatchNormalization()
+
+    def call(self, input_tensor):
+        residual = input_tensor
+        if self.stride == 2:
+            residual = self.pooling(residual)
+
+        x = self.dwconv_0(input_tensor)
+        x = self.pwconv_0(x)
+        x = self.bn_0(x)
+        x = relu(x)
+        x = self.dwconv_1(x)
+        x = self.pwconv_1(x)
+        x = self.bn_1(x)
+
+        if x.shape[-1] != residual.shape[-1]:
+            residual = tf.concat([residual, tf.zeros_like(residual)], axis=-1, name='channel_padding')
+
+        x = relu(x + residual)
+        return x
+
+
+def FeatureExtractor():
+    inputs = Input(shape=(128, 128, 3), name='Input')
+    conv = Conv2D(filters=24, kernel_size=(5, 5), strides=(2, 2), padding='same', name='Conv')(inputs)
+    x = ReLU(name='ReLU')(conv)
+    SBB_0 = BlazeBlock(24,    name='BlazeBlock_0')(x)
+    SBB_1 = BlazeBlock(24,    name='BlazeBlock_1')(SBB_0)
+    SBB_2 = BlazeBlock(48, 2, name='BlazeBlock_2')(SBB_1)
+    SBB_3 = BlazeBlock(48,    name='BlazeBlock_3')(SBB_2)
+    SBB_4 = BlazeBlock(48,    name='BlazeBlock_4')(SBB_3)
+    DBB_0 = DoubleBlazeBlock(96, 2, name='DoubleBlazeBlock_0')(SBB_4)
+    DBB_1 = DoubleBlazeBlock(96,    name='DoubleBlazeBlock_1')(DBB_0)
+    DBB_2 = DoubleBlazeBlock(96,    name='DoubleBlazeBlock_2')(DBB_1)
+    DBB_3 = DoubleBlazeBlock(96, 2, name='DoubleBlazeBlock_3')(DBB_2)
+    DBB_4 = DoubleBlazeBlock(96,    name='DoubleBlazeBlock_4')(DBB_3)
+    DBB_5 = DoubleBlazeBlock(96,    name='DoubleBlazeBlock_5')(DBB_4)
+
+    model = tf.keras.Model(inputs=inputs, outputs=[DBB_2, DBB_5], name='FeatureExtractor')
+    
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
